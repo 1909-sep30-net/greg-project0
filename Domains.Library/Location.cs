c03c4f99c2b30145dc0b3f5337343fb3a4dd707c
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-04 14:52:57 -0500

    Change Store to Location, implement at test Location

---
+++ Domains.Library/Location.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domains.Library
+{
+    /*
+     * NOTES 
+     * Id is currently a private property only accessed in the Customer constructor
+     * Any adjustment to ID must be made in constructor as it affects static nextID field
+     */
+    public class Location
+    {
+        //static fields
+        private static int nextID = 0;
+
+        //struct to connect a product with the amount in stock
+        private struct InventoryItem
+        {
+            public Product product;
+            public int quantity;
+        }
+
+        //fields
+        private string storeName;
+        private string address;
+        private int storeID;
+        private List<InventoryItem> inventory;
+
+        //Properties
+        public string StoreName
+        {
+            get { return storeName; }
+            set { storeName = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int StoreID
+        {
+            get { return storeID; }
+        }
+
+        //Constructor
+        public Location(string name, string address)
+        {
+            StoreName = name;
+            Address = address;
+            storeID = Location.nextID;
+            Location.nextID++;
+
+        }
+    }
+}
--- Store.App/Program.cs
+++ Store.App/Program.cs
@@ -8,20 +8,9 @@ namespace Store.App
     {
         static void Main(string[] args)
         {
-            //Test Customer class
-            var cust1 = new Customer("Greg", "Favrot");
-            var cust2 = new Customer("Mark", "Moore");
-            Console.WriteLine(cust1.FullName + " " + cust1.CustID);
-            Console.WriteLine(cust2.FirstName + " " + cust2.LastName + " " + cust2.CustID);
-
-            //Test Product class
-            var prod1 = new Product("book", "can be read to gain info");
-            var prod2 = new Product("coke", "");
-
-            Console.WriteLine(prod1.ProductName + " " + prod1.ProductDescription + " " + prod1.ProductID);
-            Console.WriteLine(prod2.ProductName + " " + prod2.ProductDescription + " " + prod2.ProductID);
-
-            ;
+            var store = new Location("Walmart", "123 Main St");
+            Console.WriteLine(store.StoreName);
+            Console.WriteLine(store.Address);
         }
     }
 }
