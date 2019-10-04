284bd36fe186e7fc64ee5378366bc70634851714
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-04 10:57:41 -0500

    Create Store.App and Store.Library projects

---
+++ Store.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Store.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
